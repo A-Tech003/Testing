7d7ed31f09333799a5e6d4e29178e49b07f3bc83
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-08-25 14:14:10 +0100

    Created a simple counter app for my testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Counter from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe(Counter, ( => {
+    it("counter display correct initial count")
+}))
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const Increase = () => {
+    const newValue = count + 1;
+    setCount(newValue);
+  }
+
+  const Restart = () => {
+    setCount(0);
+  }
+
+  const Decrease = () => {
+    const newValue = count - 1;
+    newValue < 1? setCount(0) : setCount(newValue);
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counting System</h1>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={Increase}>Increase</button>
+        <button onClick={Restart}>Restart</button>
+        <button onClick={Decrease}>Decrease</button>
+      </div>
     </div>
   );
 }
